1c7d23fb33495bf2ceba90cf86227b3d00fb700d
Riakol <40207895+Riakol@users.noreply.github.com>
2023-06-15 15:46:45 +0600

    Add files via upload

---
+++ "Поколение Python": курс для продвинутых/Множества/set_6.py
@@ -0,0 +1,3 @@
+fruits = {'apple', 'banana', 'cherry', 'avocado', 'pineapple', 'apricot', 'banana', 'avocado', 'grapefruit'}
+frruits_rev = sorted(fruits, reverse=True)
+[print(i) for i in frruits_rev]
