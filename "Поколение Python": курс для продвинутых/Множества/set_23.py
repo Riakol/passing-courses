f6f2871b0d56ac1d3b7ce7ba65a0edf0f5b0e21b
Riakol <40207895+Riakol@users.noreply.github.com>
2023-07-28 15:02:01 +0600

    Add files via upload

--- "Поколение Python": курс для продвинутых/Множества/set_23.py
+++ "Поколение Python": курс для продвинутых/Множества/set_23.py
@@ -1,8 +1,6 @@
 # put your python code here
 num, num2, num3 = set(int(i) for i in input().split()), set(int(i) for i in input().split()), set(int(i) for i in input().split())
 
-print(*sorted(num3 - (num | num2), reverse=True))
+score = {int(i) for i in range(11)}
 
-
-
-
+print(*sorted(score - (num | num2 | num3)))
