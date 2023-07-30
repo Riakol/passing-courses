62fda3bfbea6e963dbf05d72a02569689aafa12f
Riakol <40207895+Riakol@users.noreply.github.com>
2023-07-30 18:04:07 +0600

    Add files via upload

---
+++ "Поколение Python": курс для продвинутых/Множества/set_26.py
@@ -0,0 +1,6 @@
+words = ['Plum', 'Grapefruit', 'apple', 'orange', 'pomegranate', 'Cranberry', 'lime', 'Lemon', 'grapes', 'persimmon', 'tangerine', 'Watermelon', 'currant', 'Almond']
+words_ed = {i[0].lower() for i in words}
+print(*sorted(words_ed))
+
+
+
