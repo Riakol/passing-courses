b14898630983956688e841c6e0fa954661f1e6c2
Riakol <40207895+Riakol@users.noreply.github.com>
2023-08-18 09:01:19 +0600

    Add files via upload

---
+++ "Поколение Python": курс для продвинутых/Словарь/dict_4.py
@@ -0,0 +1,7 @@
+# put your python code here
+numbers = {0: 'zero', 1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five', 6: 'six', 7: 'seven', 8: 'eight', 9: 'nine'}
+
+n = int(input())
+
+for i in str(n):
+    print(numbers[int(i)], end=' ')
