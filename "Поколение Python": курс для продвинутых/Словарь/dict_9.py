d76c57533399b32f5b8f0dd334b109c5a6107b6a
Riakol <40207895+Riakol@users.noreply.github.com>
2023-08-23 09:02:30 +0600

    Add files via upload

---
+++ "Поколение Python": курс для продвинутых/Словарь/dict_9.py
@@ -0,0 +1,7 @@
+dict1 = {'a': 100, 'z': 333, 'b': 200, 'c': 300, 'd': 45, 'e': 98, 't': 76, 'q': 34, 'f': 90, 'm': 230}
+dict2 = {'a': 300, 'b': 200, 'd': 400, 't': 777, 'c': 12, 'p': 123, 'w': 111, 'z': 666}
+result = dict2.copy()
+
+for key, value in dict1.items():
+    if key in dict1:
+        result[key] = result.get(key, 0) + value
