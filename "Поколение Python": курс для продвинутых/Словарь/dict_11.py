906fc081230883638c9c102ae6f692f463e88f3e
Riakol <40207895+Riakol@users.noreply.github.com>
2023-08-25 09:17:32 +0600

    Add files via upload

---
+++ "Поколение Python": курс для продвинутых/Словарь/dict_11.py
@@ -0,0 +1,9 @@
+s = 'orange strawberry barley gooseberry apple apricot barley currant orange melon pomegranate banana banana orange barley apricot plum grapefruit banana quince strawberry barley grapefruit banana grapes melon strawberry apricot currant currant gooseberry raspberry apricot currant orange lime quince grapefruit barley banana melon pomegranate barley banana orange barley apricot plum banana quince lime grapefruit strawberry gooseberry apple barley apricot currant orange melon pomegranate banana banana orange apricot barley plum banana grapefruit banana quince currant orange melon pomegranate barley plum banana quince barley lime grapefruit pomegranate barley'
+
+result = {}
+
+for i in s.split():
+    if s.split().count(i) >= max([s.count(i) for i in s.split()]):
+        result[i] = result.get(i, s.split().count(i))
+
+print(sorted(result.keys())[0])
