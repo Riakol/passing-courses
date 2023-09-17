ea2606465b7c81089d29fe855246ba9541a02d03
Riakol <40207895+Riakol@users.noreply.github.com>
2023-09-17 20:58:11 +0600

    Add files via upload

---
+++ "Поколение Python": курс для продвинутых/Словарь/dict_34.py
@@ -0,0 +1,14 @@
+emails = {'nosu.edu': ['timyr', 'joseph', 'svetlana.gaeva', 'larisa.mamuk'], 
+          'gmail.com': ['ruslan.chaika', 'rustam.mini', 'stepik-best'], 
+          'msu.edu': ['apple.fruit', 'beegeek', 'beegeek.school'], 
+          'yandex.ru': ['surface', 'google'],
+          'hse.edu': ['tomas-henders', 'cream.soda', 'zivert'],
+          'mail.ru': ['angel.down', 'joanne', 'the.fame.moster']}
+
+emails_list = []
+
+for key, value in emails.items():
+    for val in value:
+        emails_list.append(val + '@' + key)
+emails_list.sort()
+print(*emails_list, sep='\n')
