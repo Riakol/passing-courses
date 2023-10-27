7205f3ac85e691b39f8673db28717385d735771b
Riakol <40207895+Riakol@users.noreply.github.com>
2023-10-27 09:24:51 +0600

    Add files via upload

---
+++ "Поколение Python": курс для продвинутых/модуль complex/complex_1.py
@@ -0,0 +1,7 @@
+# put your python code here
+cplx_1 = input()
+cplx_2 = input()
+
+print(f"({cplx_1}) + ({cplx_2}) = {complex(cplx_1) + complex(cplx_2)}")
+print(f"({cplx_1}) - ({cplx_2}) = {complex(cplx_1) - complex(cplx_2)}")
+print(f"({cplx_1}) * ({cplx_2}) = {complex(cplx_1) * complex(cplx_2)}")
