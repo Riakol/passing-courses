d79e51306bc5a4c2fbfaf2b2371b633eda867606
Riakol <40207895+Riakol@users.noreply.github.com>
2023-12-05 17:35:14 +0600

    Add files via upload

---
+++ "Поколение Python": курс для продвинутых/Функции/functions_36.py
@@ -0,0 +1,5 @@
+countries = ['Russia', 'USA', 'UK', 'Germany', 'France', 'India']
+capitals = ['Moscow', 'Washington', 'London', 'Berlin', 'Paris', 'Delhi']
+population = [145_934_462, 331_002_651, 80_345_321, 67_886_011, 65_273_511, 1_380_004_385]
+
+result = [print(f"{j} is the capital of {i}, population equal {k} people.") for i, j, k in zip(countries, capitals, population)]
