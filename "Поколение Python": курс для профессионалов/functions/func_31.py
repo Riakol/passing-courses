396f6ec5c88c21b3267e73ca3f8d938b7bdb9864
Rise A <40207895+Riakol@users.noreply.github.com>
2024-06-26 22:49:45 +0500

    Add files via upload

---
+++ "Поколение Python": курс для профессионалов/functions/func_31.py
@@ -0,0 +1,7 @@
+def sandwich(func):
+    def wrapper(*args, **kwargs):
+        print('---- Верхний ломтик хлеба ----')
+        result = func(*args, **kwargs)
+        print('---- Нижний ломтик хлеба ----')
+        return result
+    return wrapper
