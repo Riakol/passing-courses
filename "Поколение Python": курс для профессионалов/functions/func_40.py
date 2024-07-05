2731d8c4be1f311e487f750494b418849a48d76f
Rise A <40207895+Riakol@users.noreply.github.com>
2024-07-05 13:42:55 +0500

    Add files via upload

---
+++ "Поколение Python": курс для профессионалов/functions/func_40.py
@@ -0,0 +1,11 @@
+import functools
+
+def repeat(times):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            for _ in range(times):
+                result = func(*args, **kwargs)
+            return result
+        return wrapper
+    return decorator
